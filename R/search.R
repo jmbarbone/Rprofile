127c6c7e102a82f8937f2c0eb0a421e8c829b8da
jmbarbone <jmbarbone@gmail.com>
2022-03-05 12:14:46 -0500

    add .Search()
    related to #4

---
+++ R/search.R
@@ -0,0 +1,31 @@
+#' Search print
+#'
+#' A more colorful way of printing [search()]
+#'
+#' @export
+#' @returns Nothing, called for its side-effects
+#' @family Rprofile
+.Search <- function() {
+  x <- paste0(format(search()), " ")
+  n <- length(x)
+
+  res <-
+    if (n <= 2) {
+      paste0(x, collapse = "")
+    } else {
+      by <- max(2, getOption("width") %/% nchar(x[1]))
+      s <- unique(c(seq(0, n, by = by), n))
+      ends <- s[-1]
+      starts <- s[-length(s)] + 1
+
+      mapply(
+        function(start, end) {
+          paste0(x[seq.int(start, end)], collapse = "")
+        },
+        start = starts,
+        end = ends
+      )
+    }
+
+  cat(crayon_magenta(res), sep = "\n")
+}
