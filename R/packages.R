65103f0bebcc6c2b419dbabb0a9cb9cf9ab60887
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-12-26 18:23:13 -0500

    condense files

--- R/packages.R
+++ R/packages.R
@@ -64,3 +64,34 @@
   e$op$defaultPackages <- new
   assign_rprofile(e)
 }
+
+#' Open package url
+#'
+#' @param pkg A character vector of package names
+#' @returns Nothing, used for its side-effects
+#' @export
+.OpenPackageUrl <- function(pkg) {
+  names(pkg) <- pkg
+  invisible(lapply(pkg, do_open_pkg_url))
+}
+
+do_open_pkg_url <- function(x) {
+  if (!requireNamespace(x, quietly = TRUE)) {
+    msg <- sprintf("no installation found for {%s}", x)
+    message(msg)
+    return(NULL)
+  }
+
+  urls <- utils::packageDescription(x)$URL
+
+  if (is.null(urls)) {
+    msg <- sprintf("no URL found for {%s}", x)
+    warning(msg, call. = FALSE)
+    return(NULL)
+  }
+
+  urls <- strsplit(urls, ",[[:space:]]+")[[1]]
+  urls <- unique(urls)
+  lapply(urls, utils::browseURL)
+  urls
+}
