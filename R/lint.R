32922d14a24a840c3077742f82ce2320719451ab
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-03-29 23:10:21 -0400

    #21 find linters better

--- R/lint.R
+++ R/lint.R
@@ -1,12 +1,15 @@
 #' Lint a single file
 #'
-#' Use `lintr::lint_dir()` to _lint_ a single file
+#' Use `lintr::lint()` to _lint_ a single file
 #'
 #' @param path The file path.  Default (`NULL`) checks for the current file open
 #'   in the source editor in **RStudio**.
-#' @returns See `lintr::lint_dir()`
+#' @param linters Passed to `linters` argument in `lintr::lint()`; however,
+#'   when passing a character vector, finds all linters with that tag.
+#' @param ... Additional arguments passed to `lintr::lint()`
+#' @returns See `lintr::lint()`
 #' @export
-.LintFile <- function(path = NULL) {
+.LintFile <- function(path = NULL, linters = "default", ...) {
   fuj::require_namespace("lintr")
 
   if (is.null(path)) {
@@ -14,9 +17,12 @@
     path <- rstudioapi::getSourceEditorContext()$path
   }
 
+  if (is.character(linters)) {
+    linters <- lintr::available_linters(tags = linters)$linter
+    linters <- fuj::set_names(linters)
+    linters <- lapply(linters, get, pos = asNamespace("lintr"))
+  }
+
   path <- normalizePath(path, .Platform$file.sep, mustWork = TRUE)
-  dir <- dirname(path)
-  exclude_files <- list.files(dir, recursive = TRUE)
-  exclude_files <- setdiff(exclude_files, basename(path))
-  lintr::lint_dir(dir, exclusions = exclude_files)
+  lintr::lint(path, linters = linters,  ...)
 }
