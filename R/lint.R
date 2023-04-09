fb7b761ab620e4c2fbcdf2fe402ad13205a14fb0
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-04-08 23:02:28 -0400

    Update lint.R

--- R/lint.R
+++ R/lint.R
@@ -7,10 +7,22 @@
 #' @param linters Passed to `linters` argument in `lintr::lint()`; however,
 #'   when passing a character vector, finds all linters with that tag.
 #' @param ... Additional arguments passed to `lintr::lint()`
+#' @param config Global configuration file; when `NA` does nothing
 #' @returns See `lintr::lint()`
 #' @export
-.LintFile <- function(path = NULL, linters = "default", ...) {
+.LintFile <- function(
+    path = NULL,
+    linters = "default",
+    ...,
+    config = Sys.getenv("LINTR_GLOBAL_CONFIG", NA)
+) {
   fuj::require_namespace("lintr")
+
+  if (!is.na(config)) {
+    stopifnot(fs::file_exists(config))
+    op <- options(lintr.linter_file = config)
+    on.exit(options(op), add = TRUE)
+  }
 
   if (is.null(path)) {
     fuj::require_namespace("rstudioapi")
