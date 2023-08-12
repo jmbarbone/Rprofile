7feb53656aee2667a81c807ab7276d694896fb0e
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-08-12 09:51:21 -0400

    include details (#26)

--- R/news.R
+++ R/news.R
@@ -2,6 +2,10 @@
 #'
 #' Include links to github on `NEWS.md`
 #'
+#' @details URLs are detected for GitHub issue.  For example, a line with "Fixes
+#'   bug [#123]" will append the URL for the issue number 123.  The repository
+#'   is picked up from the `DESCRIPTION` file.
+#'
 #' @param path The path to your package
 #' @param ask Prompts updating `NEWS.md`
 #' @param url Optional URL to set for bug reports
