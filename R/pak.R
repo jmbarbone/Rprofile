740c0e0928371a2d8e7e27d27a292e071b547694
Jordan Mark Barbone <jmbarbone@gmail.com>
2024-12-29 23:48:58 -0500

    safer use of pak

--- R/pak.R
+++ R/pak.R
@@ -1,8 +1,22 @@
 #' Wrapper for `{pak::pak}`
-#' 
+#'
+#' @details If the envvar `R_LIBS_PAK` is set then it will be used as the
+#' library path for `{pak}`
+#'
 #' @param ... Arguments passed to [pak::pak()].
 #' @export
 .Pak <- function(...) {
   fuj::require_namespace("pak")
-  pak::pak(...)
+  fuj::require_namespace("withr")
+  
+  libs <- c(Sys.getenv("R_LIBS_PAK"), .libPaths())
+  libs <- unique(libs)
+  libs <- libs[dir.exists(libs)]
+  libs <- Filter(\(lib) length(dir(lib)) > 0, libs)
+  
+  withr::with_package(
+    package = "pak",
+    lib.loc = libs,
+    utils::getFromNamespace("pak", "pak")(...)
+  )
 }
