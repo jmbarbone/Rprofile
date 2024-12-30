81006c62c81771ac53305f9b41b3e52895ea1755
Jordan Mark Barbone <jmbarbone@gmail.com>
2024-12-29 23:48:47 -0500

    uses R directory

--- R/library.R
+++ R/library.R
@@ -3,8 +3,7 @@
 #' @export
 .UsePackageLibrary <- function() {
   fuj::require_namespace("here")
-  lib <- file.path("~", paste0(basename(here::here()), "-library"))
+  lib <- file.path("~", "R", paste0(basename(here::here()), "-library"))
   lib <- normalizePath(lib, "/", FALSE)
-  dir.create(lib, showWarnings = FALSE, recursive = TRUE)
   .libPaths(c(lib, .libPaths()))
 }
