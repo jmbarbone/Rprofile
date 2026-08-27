115b990bb6210289041248cd586361df31819825
Jordan Mark Barbone <jmbarbone@gmail.com>
2026-08-26 22:48:00 -0400

    improves namespace handling

--- R/namespace.R
+++ R/namespace.R
@@ -1,14 +1,16 @@
 reset_namespaces <- function(expr) {
-  set_equal <- function(x, y) {
-    !anyNA(match(x, y)) && !anyNA(match(y, x))
-  }
-
+  # we specifically want to load in the namespaces from R_LIBS_RPROFILE
+  set_equal <- function(x, y) !anyNA(match(x, y)) && !anyNA(match(y, x))
   loaded <- loadedNamespaces()
-  paths <- .libPaths()
+  old <- .libPaths()
+  .libPaths(
+    Sys.getenv("R_LIBS_RPROFILE", "~/R/rprofile-library"),
+    include.site = TRUE
+  )
   on.exit(
     {
-      .libPaths(paths)
-      for (attempt in seq_len(100L)) {
+      .libPaths(old)
+      for (attempt in seq_len(10L)) {
         if (set_equal(loadedNamespaces(), loaded)) {
           break
         }
@@ -18,9 +20,9 @@ reset_namespaces <- function(expr) {
         }
       }
 
-      if (attempt == 100L) {
+      if (attempt == 10L) {
         warning(
-          "Failed to unload all namespaces after 100 attempts",
+          "Failed to unload all namespaces after 10 attempts",
           call. = FALSE
         )
       }
@@ -40,12 +42,22 @@ available_namespace <- function(package, ...) {
   reset_namespaces(fuj::available_namespace(package, ...))
 }
 
-
 require_fuj <- function() {
-  if (!"fuj" %in% .packages(all.available = TRUE, lib.loc = .libPaths())) {
+  # namespace() will load the namespace which we want to avoid within this
+  # package
+  lib <- .libPaths()
+  if (!"fuj" %in% .packages(all.available = TRUE, lib.loc = lib)) {
     stop(errorCondition(
       "fuj is not installed",
       class = "namespace_error"
+    ))
+  }
+
+  version <- utils::packageVersion("fuj", lib)
+  if (version < "0.2.2.9005") {
+    stop(errorCondition(
+      sprintf("fuj >= 0.2.2.9005 required but got %s", format(version)),
+      class = "namespace_version_error"
     ))
   }
 }
